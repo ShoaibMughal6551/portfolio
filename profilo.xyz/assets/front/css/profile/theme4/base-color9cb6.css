a791467ac4454f0de7a1b51c6b09ee15263f85b7
Sami Ullah <sami_ullah71@outlook.com>
2023-02-20 18:45:01 +0500

    portfolio website

---
+++ profilo.xyz/assets/front/css/profile/theme4/base-color9cb6.css
@@ -0,0 +1,19 @@
+:root {
+    --primary-color: #F57236;
+}
+.btn-link:after {
+    background-color: #F57236;
+}
+.date-bg-one {
+    background-color: #F5723614;
+}
+.portfolio-area .portfolio-item .portfolio-img .portfolio-content {
+    background-color: #F57236E6;
+}
+.testimonial-area .testimonial-item.item-one:after {
+    color: #F572361A;
+}
+.breadcrumbs-area {
+    background: linear-gradient(
+103.3deg, #F5723666 2.43%, #F572361A 46.64%, #F572361A 100%);
+}
